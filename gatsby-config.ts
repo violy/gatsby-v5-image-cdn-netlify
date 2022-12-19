c97ddc4a9581dcd90c55e2baa8177dbe149426b5
Arthur Violy <arthur@violy.net>
2022-12-19 17:33:12 +0100

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,20 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `gatsby-v5-image-cdn-netlify`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [{
+    resolve: 'gatsby-source-wordpress',
+    options: {
+      "url": "https://wpgatsbydemo.wpengine.com/graphql"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp"]
+};
+
+export default config;
